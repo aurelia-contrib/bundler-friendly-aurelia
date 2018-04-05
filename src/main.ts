3f5affeccede1856ab719f5fb0a1b4a29578a14f
obedm503 <obedm503@gmail.com>
2018-04-05 14:13:40 -0600

    user DOM instead of PLATFORM.global.document

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 ﻿import 'reflect-metadata';
 import { WebpackLoader } from 'aurelia-loader-webpack';
 import { initialize } from 'aurelia-pal-browser';
-import { Aurelia, PLATFORM } from 'aurelia-framework';
+import { DOM, PLATFORM } from 'aurelia-pal';
+import { Aurelia } from 'aurelia-framework';
 
 import { bootstrap } from './bootstrapper';
 import { App } from './app/app';
 
 (async () => {
-  const host = PLATFORM.global.document.getElementById('root');
+  const host = DOM.getElementById('root');
   // there shouldn't be a loader, because aurelia should use standard imports
   const loader = new WebpackLoader();
   const aurelia: Aurelia = await bootstrap(host, initialize, loader);
