2fc3db10e7e74ac4cd1f94aff4b3a735249d2958
obedm503 <obedm503@gmail.com>
2018-04-05 18:59:07 -0600

    add server script

--- package-scripts.js
+++ package-scripts.js
@@ -1,17 +1,28 @@
 const { series, crossEnv, concurrent, rimraf } = require('nps-utils');
 
-module.exports = {
-  scripts: {
-    default: 'nps start',
-    start: {
-      default: `webpack-dev-server -d --inline`,
-      hmr: `webpack-dev-server -d --inline --hot`,
+module.exports.scripts = {
+  default: 'nps start',
+  start: {
+    default: concurrent({
+      client: 'nps start.client',
+      server: 'nps start.server',
+    }),
+    client: {
+      default: `webpack --watch -d --inline`,
+      hmr: `webpack --watch -d --inline --hot`,
     },
-    build: series(
-      rimraf('dist'),
-      crossEnv('NODE_ENV=production webpack --progress -p'),
-    ),
-    prettier: 'prettier --write *.[tj]s src/*.[tj]s src/**/*.[tj]s',
-    serve: 'http-server dist --cors',
+    server: [
+      'nodemon',
+      '--watch "src"',
+      '--ext "ts"',
+      '--exec "ts-node --type-check ./src/server.ts"',
+    ].join(' '),
   },
+  build: {
+    default: series(rimraf('dist'), 'nps build.client', 'nps build.server'),
+    client: crossEnv('NODE_ENV=production webpack --progress -p'),
+    server: crossEnv('NODE_ENV=production tsc --project tsconfig.json'),
+  },
+  prettier: 'prettier --write *.[tj]s src/*.[tj]s src/**/*.[tj]s',
+  serve: crossEnv('NODE_ENV=production node dist/server'),
 };
